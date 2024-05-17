d9d6771146d01a825bdee8780fa17ba84f5aaf4f
Keijo Kapp <keijo.kapp@gmail.com>
2024-05-17 17:01:46 +0300

    Changed meta database format

--- types/pouchdb-utils.d.ts
+++ types/pouchdb-utils.d.ts
@@ -5,4 +5,3 @@ export declare function clone<T>(t: T): T;
 export declare function filterChange(opts: unknown): (change: Change) => boolean | PouchError;
 export declare function invalidIdError(id: Id): asserts id is Id;
 export declare function rev(doc: PartialBy<Doc, '_rev'>, deterministicRevs: boolean): RevId;
-export declare function uuid(): string;
